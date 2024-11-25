c3801b01b037579e8b2a4d1aab4b42b8487cdee1
Ivan95ant <ivan1995ant@gmail.com>
2024-11-25 23:41:35 +0200

    clickable link

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,7 @@ const Projects = () => {
     {
       name: "Directory Synchronization Tool",
       description: "This project implements a peer-to-peer directory synchronization tool written in Python.\n It monitors a specified directory and synchronizes changes (creations, modifications, deletions, and renames) across multiple machines on a local network.",
-      details: `The tool uses UDP broadcasts for node discovery and TCP connections for file synchronization.\n It employs pyrsync2, a Python wrapper for librsync, for efficient delta encoding, ensuring that only changes within files are transmitted rather than entire files.\n Nodes establish persistent connections and use modification timestamps to determine the most recent changes, implementing a "last modified wins" conflict resolution strategy. \n
+      details: `The tool uses UDP broadcasts for node discovery and TCP connections for file synchronization.\n It employs pyrsync2, a Python wrapper for librsync, for efficient delta encoding, ensuring that only changes within files are transmitted rather than entire files.\n Nodes establish persistent connections and use modification timestamps to determine the most recent changes, implementing a "last modified wins" conflict resolution strategy. 
 GitHub: <a href="https://github.com/Ivan95ant/synched_drive.git" target="_blank" rel="noopener noreferrer">https://github.com/Ivan95ant/synched_drive.git</a>`,
     },
     {
@@ -48,11 +48,10 @@ GitHub: <a href="https://github.com/Ivan95ant/synched_drive.git" target="_blank"
 
             {/* Non-clickable section for additional details */}
             <div className={`${styles.projectDetails} ${activeIndex === index ? styles.show : ''}`}>
-                {project.details.split("\n").map((line, i) => (
-                  <p key={i}>&bull; {line}</p>
-                ))}
-              </div>
-       
+              {project.details.split("\n").map((line, i) => (
+                <p key={i} dangerouslySetInnerHTML={{ __html: `&bull; ${line}` }} />
+              ))}
+            </div>
           </li>
         ))}
       </ul>
