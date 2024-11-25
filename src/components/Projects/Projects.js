2021be66d589af9613a1e7ac4512bb4b459ae67f
Ivan95ant <ivan1995ant@gmail.com>
2024-11-25 23:35:43 +0200

    add project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,10 +13,17 @@ const Projects = () => {
         "This Website was created to practice learned material + demonstrate my skillset. \n React: Frontend. \n NodeJS + Express: Backend. \n MongoDB: Database ('Contact' feature)\n Google Maps API: 'Calculate Distance' feature. \n Modal-React: Pop-up windows (Welcome/About/Experience/Skills)\n Heroku: Backend deployment. \n Firebase: Frontend deployment.",
     },
     {
+      name: "Directory Synchronization Tool",
+      description: "This project implements a peer-to-peer directory synchronization tool written in Python.\n It monitors a specified directory and synchronizes changes (creations, modifications, deletions, and renames) across multiple machines on a local network.",
+      details: `The tool uses UDP broadcasts for node discovery and TCP connections for file synchronization.\n It employs pyrsync2, a Python wrapper for librsync, for efficient delta encoding, ensuring that only changes within files are transmitted rather than entire files.\n Nodes establish persistent connections and use modification timestamps to determine the most recent changes, implementing a "last modified wins" conflict resolution strategy. \n
+GitHub: <a href="https://github.com/Ivan95ant/synched_drive.git" target="_blank" rel="noopener noreferrer">https://github.com/Ivan95ant/synched_drive.git</a>`,
+    },
+    {
       name: "Project X",
       description: "",
       details: "To be updated soon",
     },
+    
     // Add more projects items as needed
   ];
 
